bbcd0052300d2f419fa556bfe954a7e0b426388a
codepath <chen836027055@gmail.com>
2023-05-17 04:59:07 -0400

    testing case

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -3,14 +3,28 @@ import React from 'react'
 import { render, screen } from "@testing-library/react";
 // import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
-import NewsRow from "../components/News/NewsRow";
+import News from "../components/News/News";
 
 /* This is a test that is testing the App component. 
  * It is testing that the heading is correct. */
 describe("App", () => {
   it("should have exact heading", () => {
+
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
     /* Rendering the App component. */
-    const { container } = render(<NewsRow />);
+    const { container } = render(<News />);
 
     /* Getting the element with the test id of "app-header-heading". */
     const mainHeading = screen.getByTestId("news-header-heading");
